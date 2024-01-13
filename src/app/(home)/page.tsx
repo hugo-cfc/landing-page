79d93bba94323e0817a38e204e4e77383bd5134e
Hugo Cesar <hugocfc98@gmail.com>
2024-01-13 20:15:57 -0300

    feat: AboutIt Section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Header from '../../components/Header';
 import Hero from './components/Hero';
 import Sections from './components/Sections';
 import AboutIt from './components/AboutIt';
+import CourseContent from './components/CourseContent';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Sections>
         <AboutIt />
+        <CourseContent />
       </Sections>
     </main>
   );

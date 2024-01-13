6be6cbfc94fea2d8b625bb3a12da8bfd0da6b90a
Hugo Cesar <hugocfc98@gmail.com>
2024-01-13 17:51:26 -0300

    feat: Header desktop

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes } from 'react';
 interface ContainerProps extends HTMLAttributes<HTMLDivElement> {}
 
 const Container = ({ className, children, ...rest }: ContainerProps) => {
-  const finalStyle = `w-full tablet:w-[635px] notebook:w-[712px] desktop:w-[1004px] ${className}`;
+  const finalStyle = `w-full tablet:w-[635px] notebook:w-[712px] desktop:w-[1280px] ${className}`;
 
   return (
     <div className={finalStyle} {...rest}>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,10 +11,10 @@ import SenaiLogo from '@/../../public/senaiLogo.svg';
 const Header = () => {
   return (
     <header className='py-4 px-3 flex items-center justify-center bg-primary'>
-      <Container className='flex items-center justify-between notebook:justify-start notebook:gap-x-4'>
+      <Container className='flex items-center justify-between notebook:justify-start notebook:gap-x-4 desktop:gap-x-6'>
         <Link className='z-10 cursor-pointer' href='/'>
           <Image
-            className='w-16 notebook:w-20'
+            className='w-16 notebook:w-20 desktop:w-24'
             src={SenaiLogo}
             alt='Senai Logo'
             quality={100}
@@ -22,7 +22,7 @@ const Header = () => {
           />
         </Link>
 
-        <div className='hidden tablet:flex justify-between gap-x-12 notebook:gap-x-4'>
+        <div className='hidden tablet:flex justify-between gap-x-12 notebook:gap-x-4 desktop:gap-x-6'>
           {links.map((link) => (
             <Link
               key={link.route}
